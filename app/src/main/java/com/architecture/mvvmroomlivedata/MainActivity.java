6b5f20ac98736031464c16d98c6d54fb1a058d9c
its-mingyu <lion9638@naver.com>
2022-01-06 02:24:11 +0900

    Adapter 수정, MainActivity 수정, Cardview로 수정

--- app/src/main/java/com/architecture/mvvmroomlivedata/MainActivity.java
+++ app/src/main/java/com/architecture/mvvmroomlivedata/MainActivity.java
@@ -1,14 +1,144 @@
 package com.architecture.mvvmroomlivedata;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnPlus;
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
+    private Paint p = new Paint();
+
+    private MemoDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlus = findViewById(R.id.button_add);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+
+        initSwipe();
+
+        btnPlus.setOnClickListener(v -> {
+            // title 입력 다이얼로그를 호출한다.
+            // title 입력하여 리사이클러뷰 addItem
+            final EditText edittext = new EditText(this);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Item 추가");
+            builder.setMessage("제목을 입력해 주세요.");
+            builder.setView(edittext);
+            builder.setPositiveButton("입력",
+                    (dialog, which) -> {
+                        //제목 입력, DB추가
+                        if (!edittext.getText().toString().isEmpty()) {
+                            new Thread(() -> {
+                                MemoDto memo = new MemoDto(edittext.getText().toString(),null);
+                                db.memoDao().insert(memo);
+                            }).start();
+
+                        }
+
+                    });
+            builder.setNegativeButton("취소",
+                    (dialog, which) -> {
+                        //취소버튼 클릭
+                    });
+            builder.show();
+
+        });
+
+        db = MemoDatabase.getDatabase(this);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(db);
+        recyclerView.setAdapter(adapter);
+
+        //UI 갱신 (라이브데이터 Observer 이용, 해당 디비값이 변화가생기면 실행됨)
+        db.memoDao().getAll().observe(this, new Observer<List<MemoDto>>() {
+            @Override
+            public void onChanged(List<MemoDto> data) {
+                adapter.setItem(data);
+            }
+        });
+
+
+    }
+
+    private void initSwipe() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT /* | ItemTouchHelper.RIGHT */) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            db.memoDao().delete(adapter.getItems().get(position));
+                        }
+
+                    }).start();
+
+                }else {
+                    //오른쪽으로 밀었을때.
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE ) {
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if (dX > 0) {
+                        //오른쪽으로 밀었을 때
+
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background, p);
+                        /*
+                         * icon 추가할 수 있음.
+                         */
+                        //icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon_png); //vector 불가!
+                        // RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                        //c.drawBitmap(icon, null, icon_dest, p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 }
--- app/src/main/java/com/architecture/mvvmroomlivedata/RecyclerAdapter.java
+++ app/src/main/java/com/architecture/mvvmroomlivedata/RecyclerAdapter.java
@@ -1,4 +1,85 @@
 package com.architecture.mvvmroomlivedata;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private List<MemoDto> items = new ArrayList<>();
+    private Context mContext;
+    private MemoDatabase db;
+
+    public RecyclerAdapter(MemoDatabase db){
+        this.db = db;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_maker, parent, false);
+        mContext = parent.getContext();
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(items.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public List<MemoDto> getItems(){return items;}
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvTitle;
+        private TextView tvContents;
+        private Button btnSave;
+        private int index;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.textview_title);
+            tvContents = itemView.findViewById(R.id.edittext_content);
+            btnSave = itemView.findViewById(R.id.button_save);
+
+            btnSave.setOnClickListener(view -> {
+            });
+        }
+
+        public void onBind(MemoDto memoDto, int position){
+            index = position;
+            tvTitle.setText(memoDto.getTitle());
+            tvContents.setText(memoDto.getContents());
+        }
+
+        public void editData(String contents){
+            new Thread(()->{
+                items.get(index).setContents(contents);
+                db.memoDao().update(items.get(index));
+            }).start();
+            Toast.makeText(mContext, "저장완료!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void setItem(List<MemoDto> data){
+        items = data;
+        notifyDataSetChanged();
+    }
 }
